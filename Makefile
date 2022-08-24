7133844afcc81776dbccba9685a46908d4b8b328
justingarfield <accounts@jgarfield.com>
2022-08-24 09:13:19 -0400

    Allowed passage of more CLI Args

--- Makefile
+++ Makefile
@@ -1,16 +1,12 @@
 APP_NAME="justingarfield-homeinfrastructure/github-actions-runner"
 
-.PHONY: help
-help: ## This help.
-	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
-
-.DEFAULT_GOAL := help
+.DEFAULT_GOAL := build
 
 build: ## Build the image
-	docker build -t $(APP_NAME) docker
+  docker build -t $(APP_NAME) .
 
-shell: ## Creates a shell inside the container for debug purposes
-	docker run -it $(APP_NAME) bash
+run:
+  docker run --env-file .env -it $(APP_NAME) .
 
-shell-compose: ## Creates a shell inside the docker-compose service for debug purposes
-	docker compose run --rm runner bash
+compose:
+  docker compose .
--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token" | jq .token --raw-output)
+REGISTRATION_TOKEN=$(curl -sX POST -H "Authorization: token ${PAT}" "https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token" | jq .token --raw-output)
 
 cd /home/docker/actions-runner || exit
 
-./config.sh --url "https://github.com/${ORGANIZATION}" --token "${REG_TOKEN}"
+./config.sh \
+  --url "https://github.com/${ORGANIZATION}" \
+  --token "${REGISTRATION_TOKEN}" \
+  --name "${NAME}" \
+  --runnergroup "${RUNNER_GROUP}" \
+  --labels "${LABELS}" \
+  --work "${WORK}"
 
 cleanup() {
     echo "Removing runner..."
